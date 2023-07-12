f47bd4ec139f118a627f79154db7e8d4c9133bd9
Marta B <38409011+MartaBento@users.noreply.github.com>
2023-07-12 20:28:41 +0100

    Update wizard.tsx

--- components/wizard/wizard.tsx
+++ components/wizard/wizard.tsx
@@ -119,6 +119,7 @@ function Wizard() {
   };
 
   const shouldShowConfetti =
+    typeof window !== "undefined" &&
     !localStorage.getItem(RECOMMENDATIONS_KEY) &&
     !bookInformationError &&
     !recommendationsError;
