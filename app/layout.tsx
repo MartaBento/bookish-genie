76dcbf2f3e6165609f1c0a986335e24f7e3ba419
Marta B <38409011+MartaBento@users.noreply.github.com>
2023-08-09 18:56:49 +0100

    fix: updated the GTM script

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,15 +37,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer',${GTM_ID});
-          `}
-        </Script>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
@@ -66,6 +57,15 @@ export default function RootLayout({ children }: RootLayoutProps) {
             }}
           />
         </body>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       </html>
     </>
   );
